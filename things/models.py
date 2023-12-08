42d38be2f96c19466fe56413ff73cf88d454d7cd
Ishika <ishikadav03@gmail.com>
2023-12-08 07:12:11 +0000

    fix issues

--- things/models.py
+++ things/models.py
@@ -3,5 +3,11 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=120, unique=False, blank=True)
-    quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
+    description = models.CharField(blank=True, max_length=120, unique = False)
+    quantity = models.IntegerField(
+        unique = False,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
--- things/views.py
+++ things/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def simple_page(request):
-    return render(request, 'things/things.html')
+
+def index(request):
+    return render(request, 'home.html')
